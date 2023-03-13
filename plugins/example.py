3ce77dd39674a92ae07044e82841eb464c5ae5f0
Alyce <Alyceosbourne@outlook.com>
2023-03-13 13:41:51 +0000

    Fixed up some stuffs

--- plugins/example.py
+++ plugins/example.py
@@ -10,5 +10,13 @@ class Example(Plugin):
         print("Example plugin updated", state_manager)
 
 
+class ExamplePlugin2(Plugin):
+    def __init__(self, app):
+        super().__init__(app)
+        print("Example plugin 2 initialized")
+
+    def update(self):
+        print("Example plugin 2 updated")
+
 def setup(app):
-    return [Example(app)]
+    return [Example(app), ExamplePlugin2(app)]
