de2a66089508c8c5e0a6b34e6d198c210228a154
Alyce <Alyceosbourne@outlook.com>
2023-03-13 21:36:39 +0000

    Documented everything

--- plugins/example.py
+++ plugins/example.py
@@ -1,11 +1,28 @@
 from app_data.state_manager import State
 from app_data.plugin_loader import Plugin
-
-class TestPlugin(Plugin):
-    name ='test'
-    test_state = State('test_state')
+import ctypes
 
 
+class POINT(ctypes.Structure):
+    _fields_ = [("x", ctypes.c_long), ("y", ctypes.c_long)]
+
+
+class MousePlugin(Plugin):
+    """A plugin that gets the mouse position"""
+
+    name = "mouse"
+    position = State((0, 0))
+
+    def update(self):
+        """Gets the mouse position"""
+        pt = POINT()
+        ctypes.windll.user32.GetCursorPos(ctypes.byref(pt))
+        self.position = (pt.x, pt.y)
+
+# plugin that logs the mouse position
+class MousePosLogger(Plugin):
+    def update(self, mouse_position):
+        print(mouse_position)
 
 def setup():
-    return [TestPlugin()]
+    return (MousePlugin(),)
